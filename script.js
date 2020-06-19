db2e5f03e8a6562200a133306cc3a22dec960f50
Zane Deupree <zdtexan@gmail.com>
2020-06-18 22:03:47 -0500

    first push
    first push

---
+++ script.js
@@ -0,0 +1,32 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+alert("Welcome to my password generator!");
+alert("Follow the steps to generate a secure password! Selcting Yes will geenerate a more secure password!");
+
+
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function generatePassword() {
+  var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
